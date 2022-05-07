52452ea0dc76e523f02cadaa4998e53ef85d71ba
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-07 15:55:30 +0300

    Create change.sh

---
+++ change.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /tmp/rom
+
+export ROM_LINK="https://github.com/PixelOS-Pixelish/manifest"
+export ROM_BRANCH="twelve"
+export DEVICE_BRANCH="pos"
+export DEVICE_LUNCH="aosp_lava-userdebug"
+export BUILD_FLAG0="mka"
+export BUILD_FLAG1="bacon"
