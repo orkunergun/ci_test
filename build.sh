69312da8a56ff8c94f9ccd7d00a11d6aa01b6a15
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-07 17:45:33 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-# Source Vars
-source $CONFIG
-
 cd /tmp/rom
+
 source build/envsetup.sh
-lunch $DEVICE_LUNCH
-$BUILD_FLAG0 sepolicy && $BUILD_FLAG0 bootimage && $BUILD_FLAG0 $BUILD_FLAG1
+lunch cherish_lava-userdebug
+
+export CCACHE_DIR=/tmp/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 10G
+ccache -o compression=true
+ccache -s
+
+export KBUILD_BUILD_USER=orkunergun
+export KBUILD_BUILD_HOST=orkunergun
+export BUILD_USERNAME=orkunergun
+export BUILD_HOSTNAME=orkunergun
+export TZ=Asia/Kolkata
+
+#m otapackage & sleep 1h ; kill $!
+
+mka otapackage -j30 && cd $OUT && ls
+curl -T *-ota-*.zip https://oshi.at/ccache.zip/129600
+
+cd -
