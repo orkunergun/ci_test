031ca8559bb25df3c0cd9781eddb7b3f5f111cf5
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-07 21:49:42 +0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -8,6 +8,6 @@ git config --global user.name "Orkun Ergun"
 git config --global color.ui true
 
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest -b twelve-one -g default,-mips,-darwin,-notdefault
-git clone https://gitlab.com/68orkun/local_manifest.git --depth 1 -b A12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mt6768floslab/local_manifest.git --depth 1 -b lava-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
