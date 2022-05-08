71246723de1f1bfb82b75542676754dd15e153bd
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-08 16:29:26 +0300

    [skip CI]

---
+++ change.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /tmp/rom
+
+export ROM_LINK="https://github.com/ProjectBlaze/manifest"
+export ROM_BRANCH="12.1"
+export DEVICE_BRANCH="blaze"
+export DEVICE_LUNCH="blaze_lava-userdebug"
+export BUILD_FLAG0="brunch"
+export BUILD_FLAG1="lava"
