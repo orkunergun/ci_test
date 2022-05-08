59076785fe66e7d05f443077c335c196c60034ee
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-08 11:59:01 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ cd /tmp/rom
 source build/envsetup.sh
 lunch $DEVICE_LUNCH
 $BUILD_FLAG0 sepolicy && $BUILD_FLAG0 bootimage && $BUILD_FLAG0 $BUILD_FLAG1
+
+# upload function for uploading rom zip file! I don't want unwanted builds in my google drive haha!
+up(){
+	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+	# 14 days, 10 GB limit
+}
