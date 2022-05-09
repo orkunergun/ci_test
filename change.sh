042d141352db01726889981538d1f9141b401bee
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-09 10:45:37 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -7,3 +7,7 @@ export DEVICE_BRANCH="blaze"
 export DEVICE_LUNCH="blaze_lava-userdebug"
 export BUILD_FLAG0="brunch"
 export BUILD_FLAG1="lava"
+export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export ALLOW_MISSING_DEPENDENCIES=true
+export TARGET_GAPPS_ARCH=arm64
