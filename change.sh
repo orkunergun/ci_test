486bfce6c0fef81883ffea92650a5dffd4c3f795
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-09 12:35:13 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/ProjectBlaze/manifest"
-export ROM_BRANCH="12.1"
-export DEVICE_BRANCH="blaze"
-export DEVICE_LUNCH="blaze_lava-userdebug"
-export BUILD_FLAG0="brunch"
-export BUILD_FLAG1="lava"
+export ROM_LINK="https://github.com/CipherOS/android_manifest"
+export ROM_BRANCH="twelve-L"
+export DEVICE_BRANCH="cipher"
+export DEVICE_LUNCH="cipher_lava-userdebug"
+export BUILD_FLAG0="mka"
+export BUILD_FLAG1="bacon"
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export ALLOW_MISSING_DEPENDENCIES=true
