7d8389c9409c0a4e48c7732e7a3e84e021c811c4
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-09 21:14:44 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -8,6 +8,4 @@ export DEVICE_LUNCH="aosp_lava-userdebug"
 export BUILD_FLAG0="mka"
 export BUILD_FLAG1="bacon"
 export SELINUX_IGNORE_NEVERALLOWS=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export ALLOW_MISSING_DEPENDENCIES=true
 export TARGET_GAPPS_ARCH=arm64
