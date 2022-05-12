57685a3a537aedb8950528120bf718a3c006e7b5
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-12 16:19:00 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/ProjectBlaze/manifest"
-export ROM_BRANCH="12.1"
-export DEVICE_BRANCH="blaze"
-export DEVICE_LUNCH="blaze_lava-userdebug"
-export BUILD_FLAG0="brunch"
-export BUILD_FLAG1="lava"
+export ROM_LINK="https://github.com/Evolution-X/manifest"
+export ROM_BRANCH="elle"
+export DEVICE_BRANCH="evo11"
+export DEVICE_LUNCH="evolution_lava-userdebug"
+export BUILD_FLAG0="mka"
+export BUILD_FLAG1="evolution"
 export SELINUX_IGNORE_NEVERALLOWS=true
