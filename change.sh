62478d439c29efa834a940a3cb7f1a495ffbae0e
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-13 18:53:47 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -2,8 +2,8 @@
 cd /tmp/rom
 
 export ROM_LINK="https://github.com/Evolution-X/manifest"
-export ROM_BRANCH="elle"
-export DEVICE_BRANCH="evo11"
+export ROM_BRANCH="snow"
+export DEVICE_BRANCH="evo"
 export DEVICE_LUNCH="evolution_lava-userdebug"
 export BUILD_FLAG0="mka"
 export BUILD_FLAG1="evolution"
