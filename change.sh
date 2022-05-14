e0f884d65093337d56ffad2e94c503505f9910e4
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-14 12:16:48 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/ProjectBlaze/manifest"
-export ROM_BRANCH="12.1"
-export DEVICE_BRANCH="blaze"
-export DEVICE_LUNCH="blaze_lava-userdebug"
-export BUILD_FLAG0="brunch"
-export BUILD_FLAG1="lava"
+export ROM_LINK="https://github.com/PixysOS/manifest"
+export ROM_BRANCH="twelve"
+export DEVICE_BRANCH="pixys"
+export DEVICE_LUNCH="pixys_lava-userdebug"
+export BUILD_FLAG0="make"
+export BUILD_FLAG1="pixys"
 export SELINUX_IGNORE_NEVERALLOWS=true
