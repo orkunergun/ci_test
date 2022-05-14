9a05b0027a5880a3ffa55477a1541f34fe4a22d0
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-14 19:44:11 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/ProjectBlaze/manifest"
+export ROM_LINK="https://github.com/crdroidandroid/android"
 export ROM_BRANCH="12.1"
-export DEVICE_BRANCH="blaze"
-export DEVICE_LUNCH="blaze_lava-userdebug"
+export DEVICE_BRANCH="crdroid"
+export DEVICE_LUNCH="lineage_lava-userdebug"
 export BUILD_FLAG0="brunch"
 export BUILD_FLAG1="lava"
 export SELINUX_IGNORE_NEVERALLOWS=true
