dd8f44833f6e1a8d0e83d99f552fd006857fc8a5
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-14 18:37:16 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/PixelPlusUI-SnowCone/manifest"
-export ROM_BRANCH="snowcone-12.1"
-export DEVICE_BRANCH="ppui"
+export ROM_LINK="https://github.com/Project-Elixir/manifest"
+export ROM_BRANCH="snow"
+export DEVICE_BRANCH="Elixir"
 export DEVICE_LUNCH="aosp_lava-userdebug"
 export BUILD_FLAG0="mka"
 export BUILD_FLAG1="bacon"
-export PPUI_MAINTAINER=Orkun
+export ELIXIR_MAINTAINER=Orkun
