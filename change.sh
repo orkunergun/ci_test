2da94ffdf3ee2770a3f7a2494c176a651c7554fb
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-15 19:00:05 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/Spark-Rom/manifest"
-export ROM_BRANCH="spark"
-export DEVICE_BRANCH="spark"
-export DEVICE_LUNCH="spark_lava-userdebug"
-export BUILD_FLAG0="mka"
-export BUILD_FLAG1="bacon"
+export ROM_LINK="https://github.com/StyxProject/android"
+export ROM_BRANCH="S"
+export DEVICE_BRANCH="styx"
+export DEVICE_LUNCH="styx_lava-userdebug"
+export BUILD_FLAG0="m"
+export BUILD_FLAG1="styx-ota"
 export SELINUX_IGNORE_NEVERALLOWS=true
