9dfd0c73dc3d83e4fce4c145d9066abe2da4394a
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-19 12:05:32 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/Spark-Rom/manifest"
-export ROM_BRANCH="spark"
-export DEVICE_BRANCH="spark"
-export DEVICE_LUNCH="spark_lava-userdebug"
+export ROM_LINK="https://github.com/Komodo-OS/manifest"
+export ROM_BRANCH="12.2"
+export DEVICE_BRANCH="komodo"
+export DEVICE_LUNCH="komodo_lava-userdebug"
 export BUILD_FLAG0="mka"
-export BUILD_FLAG1="bacon"
+export BUILD_FLAG1="komodo"
 export SELINUX_IGNORE_NEVERALLOWS=true
