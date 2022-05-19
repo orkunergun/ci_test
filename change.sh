1874d8d71b3e0f6d3367e96151a1472b50bf5e12
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-19 16:00:18 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/PixelOS-Pixelish/manifest"
+export ROM_LINK="https://github.com/AOSPK/manifest"
 export ROM_BRANCH="twelve"
-export DEVICE_BRANCH="pos"
+export DEVICE_BRANCH="aospk"
 export DEVICE_LUNCH="aosp_lava-userdebug"
-export BUILD_FLAG0="mka"
+export BUILD_FLAG0="make"
 export BUILD_FLAG1="bacon"
 export SELINUX_IGNORE_NEVERALLOWS=true
