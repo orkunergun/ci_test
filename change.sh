a20113187079c96c3d5fd7e7480a6c5b03774838
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-19 15:04:42 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/ProjectBlaze/manifest"
-export ROM_BRANCH="12.1"
-export DEVICE_BRANCH="blaze"
-export DEVICE_LUNCH="blaze_lava-userdebug"
+export ROM_LINK="https://github.com/PotatoProject/manifest"
+export ROM_BRANCH="frico_mr1-release"
+export DEVICE_BRANCH="posp"
+export DEVICE_LUNCH="potato_lava-userdebug"
 export BUILD_FLAG0="brunch"
 export BUILD_FLAG1="lava"
 export SELINUX_IGNORE_NEVERALLOWS=true
