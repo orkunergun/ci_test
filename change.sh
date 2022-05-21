5af345e5d20b6eeb34448c53e60817ca11707c1f
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-21 20:27:29 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -8,3 +8,5 @@ export DEVICE_LUNCH="aosp_lava-userdebug"
 export BUILD_FLAG0="mka"
 export BUILD_FLAG1="bacon"
 export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export ALLOW_MISSING_DEPENDENCIES=true
