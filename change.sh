5bdb17cb783fe97e18fa5b6607a4e389fb52394e
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-24 18:34:28 +0300

    [skip CI]

--- change.sh
+++ change.sh
@@ -3,7 +3,7 @@ cd /tmp/rom
 
 export ROM_LINK="https://github.com/ArrowOS/android_manifest"
 export ROM_BRANCH="arrow-12.1"
-export DEVICE_BRANCH="arrow"
+export DEVICE_BRANCH="A12"
 export DEVICE_LUNCH="arrow_lava-userdebug"
 export BUILD_FLAG0="m"
 export BUILD_FLAG1="bacon"
