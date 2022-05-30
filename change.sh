b6e2a19d1a6c0c9add1103e0bcbca11b7247d86a
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-05-30 17:27:15 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/PixelOS-Pixelish/manifest"
-export ROM_BRANCH="twelve"
+export ROM_LINK="https://github.com/PixelExtended/manifest"
+export ROM_BRANCH="snow"
 export DEVICE_BRANCH="pos"
 export DEVICE_LUNCH="aosp_lava-userdebug"
 export BUILD_FLAG0="mka"
