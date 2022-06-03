74c2080ad2628e66a571f783af2c08103e24c779
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-06-03 20:48:58 +0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -10,6 +10,6 @@ git config --global user.name "Orkun Ergün"
 git config --global color.ui true
 
 # sync rom
-repo init --depth=1 --no-repo-verify -u $ROM_LINK -b $ROM_BRANCH -g default,-mips,-darwin,-notdefault
+repo init --depth=1 -u-u $ROM_LINK -b $ROM_BRANCH -g default 
 git clone https://github.com/r9lab/local_manifest.git --depth 1 -b $DEVICE_BRANCH .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -q -j8
