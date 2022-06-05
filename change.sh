191f22391aca705fe038a7d533ebc4de669f37ad
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-06-05 14:27:23 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/ProjectBlaze/manifest"
-export ROM_BRANCH="12.1"
-export DEVICE_BRANCH="blaze"
-export DEVICE_LUNCH="blaze_lava-userdebug"
-export BUILD_FLAG0="brunch"
-export BUILD_FLAG1="lava"
+export ROM_LINK="https://github.com/xdroid-oss/xd_manifest"
+export ROM_BRANCH="twelve"
+export DEVICE_BRANCH="xd"
+export DEVICE_LUNCH="xdroid_lava-userdebug"
+export BUILD_FLAG0="make"
+export BUILD_FLAG1="xd"
 export SELINUX_IGNORE_NEVERALLOWS=true
