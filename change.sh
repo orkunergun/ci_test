739684d1cd30f62ee605b25a9a0f85ac9a7dfc52
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-06-09 18:28:53 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -3,7 +3,7 @@ cd /tmp/rom
 
 export ROM_LINK="https://github.com/Project-Awaken/android_manifest"
 export ROM_BRANCH="12.1"
-export DEVICE_BRANCH="awaken"
+export DEVICE_BRANCH="L12"
 export DEVICE_LUNCH="awaken_lava-userdebug"
 export BUILD_FLAG0="make"
 export BUILD_FLAG1="bacon"
