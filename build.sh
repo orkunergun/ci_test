8bb39047c45100be139a34d5be970ddc97b09fe3
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-06-11 10:48:58 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,19 @@
 # Source Vars
 source $CONFIG
 
+# Setup ccache
+ccache_configuration() {
+	export CCACHE_DIR=/tmp/ccache
+	export CCACHE_EXEC=$(which ccache)
+	export USE_CCACHE=1
+	cat > ${CCACHE_DIR}/ccache.conf <<EOF
+depend_mode = true
+file_clone = true
+limit_multiple = 0.9
+max_size = 0
+compression = false
+hash_dir = false
+
 cd /tmp/rom
 source build/envsetup.sh
 lunch $DEVICE_LUNCH
