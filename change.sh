e5c34d3305988ba1ea68c06cdcccf3805bd09850
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-06-28 15:04:12 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/ProjectBlaze/manifest"
-export ROM_BRANCH="12.1"
-export DEVICE_BRANCH="blaze"
-export DEVICE_LUNCH="blaze_lava-userdebug"
+export ROM_LINK="https://github.com/VoltageOS/manifest"
+export ROM_BRANCH="12l"
+export DEVICE_BRANCH="VoltageOS-12.1"
+export DEVICE_LUNCH="voltage_lava-userdebug"
 export BUILD_FLAG0="brunch"
 export BUILD_FLAG1="lava"
-export SELINUX_IGNORE_NEVERALLOWS=true
