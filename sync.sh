2101c73333cbcbb4101295147de241adf5de0839
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-06-28 13:45:49 +0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -10,6 +10,6 @@ git config --global user.name "Orkun Ergün"
 git config --global color.ui true
 
 # sync rom
-repo init -u https://github.com/crdroidandroid/android.git -b 
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/r9lab/local_manifest.git --depth 1 -b $DEVICE_BRANCH .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -q -j8
